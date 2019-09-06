85877b815d34e8a673fc5abb50f5c99dc6a2f472
Ali-Lenovo <abiriali78@gmail.com>
2019-09-06 17:12:35 +0430

    first row added.

---
+++ lib/components/optionListBtn.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ddBtn extends StatefulWidget {
+  @override
+  _ddBtnState createState() => _ddBtnState();
+}
+
+class _ddBtnState extends State<ddBtn> {
+  String tripType = "One way";
+
+  void _changeTripMode() {
+    if (tripType == "One way") {
+      setState(() {
+        tripType = "Two way";
+      });
+    } else {
+      setState(() {
+        tripType = "One way";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          RaisedButton.icon(
+            icon: Icon(Icons.mode_edit),
+            label: Text("Trip type:" + tripType),
+            onPressed: _changeTripMode,
+            color: Colors.orange,
+            textColor: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './components/optionListBtn.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -11,37 +13,51 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: null,
         floatingActionButton: null,
-        body: Column(children: <Widget>[
-          Stack(children: <Widget>[
-            Image.asset('assets/images/travel-photography.jpg'),
-            Positioned(
-              bottom: 8,
-              right: 16,
-              child: Text(
-                "someText",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-                ),
-            )
-          ],),
-          Container(
-            child: Card(
-              color: Colors.white,
+        backgroundColor: Colors.red,
+        body: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Image.asset('assets/images/travel-photography.jpg'),
+                Positioned(
+                  bottom: 8,
+                  left: 16,
+                  child: Text(
+                    "What is your destination?",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+            Expanded(
               child: Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Text("hello world")
-                  ],
+                child: Card(
+                  margin: EdgeInsets.all(0),
+                  color: Colors.white,
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Expanded(child: ddBtn(),),
+                            Icon(Icons.person, color: Colors.blueGrey),
+                            Icon(Icons.credit_card, color: Colors.blueGrey),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
-          )
-        ],),
+            )
+          ],
+        ),
       ),
     );
   }
