aa9837bc448230b52f3d7cd8998b2f7b139e72eb
Ali-Lenovo <abiriali78@gmail.com>
2019-09-09 10:47:50 +0430

    Rounded bottom sheet created.

--- lib/components/persianCalendar.dart
+++ lib/components/persianCalendar.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
+import '../screens/page_view/screen1.dart';
+import '../screens/page_view/screen2.dart';
+
 class persianCalendar extends StatefulWidget {
   @override
   _persianCalendarState createState() => _persianCalendarState();
 }
 
 class _persianCalendarState extends State<persianCalendar> {
+  final controller = PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(
-        "hello Persian Calendar"
-      )
+      color: Color(0xFF737373),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25),
+              topRight: Radius.circular(25),
+            )),
+        child: PageView(
+          controller: controller,
+          children: <Widget>[
+            screen1(),
+            screen2(),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/page_view/screen1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class screen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("screen1")
+    );
+  }
+}
